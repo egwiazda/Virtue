21fca39aa7b71a5c7cd20002f7ac402947b1f29d
egwiazda <egwiazda@gmail.com>
2017-06-17 00:16:54 -0300

    added files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'app-edu',
+		wp_ws_url : 'http://webuti.url.ph/wp-appkit-api/app-edu',
+		wp_url : 'http://webuti.url.ph',
+		theme : 'q-android',
+		version : '1.0',
+		app_title : 'App Edu',
+		app_platform : 'android',
+		gmt_offset : -3,
+		debug_mode : 'off',
+		auth_key : ']KTzj8HK`ca[02*O*dD8<iZEN1WRihQ1MG1x1<c+&}VAnz{n?x}4-|san;&qs6~/',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
